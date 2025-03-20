b82f84c159343a0b0837d58dae284f39c589ce00
MasFlam <masflam@masflam.com>
2025-03-21 00:56:47 +0100

    Add monero address validation util

--- monerochad/common.py
+++ monerochad/common.py
@@ -1,4 +1,5 @@
 import discord
+import re
 
 ORANGE = discord.Color.from_str("0xff6600")
 GREEN = discord.Color.from_str("0x3de500")
@@ -12,6 +13,11 @@ COIN_GECKO_LOGO_URL = "https://static.coingecko.com/s/thumbnail-007177f3eca19695
 AUTHOR_MENTION: str = "<@141628425835118592>"
 
 DONATE_ADDRESS: str = "89oNRKJhbnHEsCy4Nu6qTGaqVJ2q8o9fuQLSWxc6X959AncFWtaHEjiRPBBnyYpknLYXSmf88Fyo4hwZmmpJw3FaHFy43F4"
+
+MONERO_ADDRESS_REGEX = re.compile(r"^[123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz]{95}$")
+
+def is_valid_monero_address(xmr_address: str) -> bool:
+	return MONERO_ADDRESS_REGEX.fullmatch(xmr_address) is not None
 
 def fail_embed(desc: str | None = None) -> discord.Embed:
 	return discord.Embed(color=RED, title=desc)
